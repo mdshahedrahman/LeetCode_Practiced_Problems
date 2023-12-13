a4da00625f6464d061f6802c522cf4423b8d5924
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2023-12-13 13:56:40 -0600

    Add files via upload

---
+++ Basic Problems/83. Remove_Duplicates_From_Sorted_list.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        unique_values = set()
+        unique_head = ListNode()
+        current = unique_head
+
+        while head:
+            if head.val not in unique_values:
+                unique_values.add(head.val)
+                current.next = ListNode(head.val)
+                current = current.next
+
+            head = head.next
+
+        return unique_head.next
