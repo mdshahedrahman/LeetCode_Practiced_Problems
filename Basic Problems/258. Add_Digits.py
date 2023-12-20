b2a1b4a375de75a1dbff6851b00355e500170777
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2023-12-19 19:20:23 -0600

    Add files via upload

---
+++ Basic Problems/258. Add_Digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        remainder = num % 9
+        if num ==0:
+            return 0
+        elif remainder == 0 and num > 0:
+            return 9
+        else:
+            return remainder
