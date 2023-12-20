3ef37a72a021c38563196f15809a1585583c59af
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2023-12-19 18:15:15 -0600

    Add files via upload

---
+++ Basic Problems/66. Plus_one.py
@@ -0,0 +1,6 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        number = int(''.join(map(str, digits)))
+        number = number + 1
+        digits_array = list(map(int, str(number)))
+        return digits_array
