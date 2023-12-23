a7e392d549f42800dd9d3a75f1caf16292817612
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2023-12-22 19:25:48 -0600

    Add files via upload

---
+++ Basic Problems/414. Third_Maximum_number.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        # Sort the array in descending order
+        sorted_arr = sorted(set(nums), reverse=True)
+
+        # Pop the third-largest number
+        if len(sorted_arr) >= 3:
+            third_largest = sorted_arr.pop(2)
+            print("Original array:", nums)
+            print("Array after popping the third-largest number:", sorted_arr)
+            return third_largest
+        elif len(sorted_arr) <= 2:
+            third_largest = sorted_arr.pop(0)
+            return third_largest
+        else:
+            third_largest = sorted_arr.pop(0)
+            return third_largest
