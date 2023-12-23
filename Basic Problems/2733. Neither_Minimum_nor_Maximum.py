d182c48aaa1d4bf9491f53d8fb8dc6eb02840f5b
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2023-12-22 19:38:37 -0600

    Add files via upload

---
+++ Basic Problems/2733. Neither_Minimum_nor_Maximum.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def findNonMinOrMax(self, nums: List[int]) -> int:
+        for element in nums:
+            if element > min(nums) and element < max(nums):
+                return element
+                break
+        return -1
