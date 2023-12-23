69bcfa6a447c5bedd4eab1a6103cf8ab42198efd
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2023-12-22 18:24:22 -0600

    Add files via upload

---
+++ Basic Problems/28. Find_the_Index_of_the_First_Occurrence_in_a_String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        index = haystack.find(needle)
+
+        # Check if the needle is found
+        if index != -1:
+            return index
+        else:
+            return -1
