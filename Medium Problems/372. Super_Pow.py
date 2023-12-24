a9f1d6e3c8f4669d35156a6ab9c793d815b2a0d8
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2023-12-23 19:29:55 -0600

    Add files via upload

---
+++ Medium Problems/372. Super_Pow.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        result_integer = int(''.join(map(str, b)))
+        power = pow(a, result_integer, 1337)
+
+        return power
