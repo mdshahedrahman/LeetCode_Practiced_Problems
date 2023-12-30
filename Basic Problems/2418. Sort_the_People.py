106b4a4de5d7a4a29acc1e6acc4f1592e0ff6b0b
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2023-12-29 21:21:58 -0600

    Add files via upload

---
+++ Basic Problems/2418. Sort_the_People.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        sorted_height = sorted(heights, reverse=True)
+        temp = []
+        temp2 = []
+        for i in sorted_height:
+            index_max = heights.index(i)
+            temp2.append(index_max)
+
+        for i in temp2:
+            temp.append(names[i])
+
+        return temp
