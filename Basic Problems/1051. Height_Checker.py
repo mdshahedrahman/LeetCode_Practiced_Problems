db08603a346a58b76dc51c33e70e2e81ffd8bf87
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2024-01-06 20:09:33 -0600

    Add files via upload

---
+++ Basic Problems/1051. Height_Checker.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        sorted_heights = sorted(heights)
+        counter = 0
+
+        for i in range(len(heights)):
+            if heights [i] != sorted_heights [i] :
+                counter = counter + 1
+
+        return counter
