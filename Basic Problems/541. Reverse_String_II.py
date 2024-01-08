53140d65f1a8c116cdaba7b9d127622c7a7afa31
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2024-01-08 13:02:51 -0600

    Add files via upload

---
+++ Basic Problems/541. Reverse_String_II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        # Extract two characters at a time using list comprehension
+        result = [s[i:i + k] for i in range(0, len(s), k)]
+
+        temp = []
+
+        for index, value in enumerate(result):
+            if index % 2 == 0:
+                reversed_value = value[::-1]
+                temp.append(reversed_value)
+            else:
+                temp.append(value)
+
+        merged_string = ''.join(temp)
+
+        return merged_string
