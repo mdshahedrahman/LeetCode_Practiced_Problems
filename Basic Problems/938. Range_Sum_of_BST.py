b15e9110e629e0ddfa0a440e47c4a7e05844985e
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2024-01-08 12:04:39 -0600

    Add files via upload

---
+++ Basic Problems/938. Range_Sum_of_BST.py
@@ -0,0 +1,29 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        def in_order_traversal(node, result):
+            if node is not None:
+                in_order_traversal(node.left, result)
+                result.append(node.val)
+                in_order_traversal(node.right, result)
+
+        sorted_nodes = []
+        temp = []
+
+        in_order_traversal(root, sorted_nodes)
+
+        for i in sorted_nodes:
+            if isinstance(i, int) and low <= i <= high:
+                temp.append(i)
+
+        sum_temp = sum(temp)
+
+        return sum_temp
