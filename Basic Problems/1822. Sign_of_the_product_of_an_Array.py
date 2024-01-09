5e80db6b73059b739595bfb20bbadc060322a1c8
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2024-01-09 10:26:04 -0600

    Add files via upload

---
+++ Basic Problems/1822. Sign_of_the_product_of_an_Array.py
@@ -0,0 +1,23 @@
+from typing import List
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        mul = 1
+        count_neg = 0
+        count_pos = 0
+
+        for i in nums:
+            mul = mul * i
+
+        if mul != 0:
+            for i in nums:
+            #    temp.append(i)
+                if i < 0:
+                    count_neg = count_neg + 1
+                elif i > 0:
+                    count_pos = count_pos + 1
+            if count_neg % 2 == 0:
+                return (1)
+            else: return(-1)
+
+        elif mul == 0:
+            return (mul)
