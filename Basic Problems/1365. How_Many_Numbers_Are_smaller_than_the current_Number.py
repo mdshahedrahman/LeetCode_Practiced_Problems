ec65694ab979b97592c4879cf7403b85d72fa8c1
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2024-01-17 14:50:01 -0600

    Add files via upload

---
+++ Basic Problems/1365. How_Many_Numbers_Are_smaller_than_the current_Number.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        count_dict = {}
+
+        # Count occurrences of each number
+        for num in nums:
+            count_dict[num] = count_dict.get(num, 0) + 1
+
+        # Calculate the number of elements smaller than each element
+        result = [sum(count_dict[key] for key in count_dict if key < num) for num in nums]
+
+        return result
+
+
+# Example usage:
+nums = [8, 1, 2, 2, 3]
+solution = Solution()
+result = solution.smallerNumbersThanCurrent(nums)
+print(result)
