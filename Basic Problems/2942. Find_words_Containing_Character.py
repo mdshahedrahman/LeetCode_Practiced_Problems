7455b6d47062c25d1bfd6aa7c3c15ce5ab5eae5e
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2024-01-22 14:37:49 -0600

    Add files via upload

---
+++ Basic Problems/2942. Find_words_Containing_Character.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        temp = []
+        # Iterate through each word in the list
+        for index, word in enumerate(words):
+            if x in word:
+                temp.append(index)
+
+        return temp
