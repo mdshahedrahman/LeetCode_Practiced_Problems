92182bcc30424b6a1f92d7e5553b0581490f5c50
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2024-02-12 15:31:52 -0600

    Time: 41 ms (25.20%), Space: 16.6 MB (70.00%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -0,0 +1,14 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        temp = []
+
+        for i in range(10):
+            mul = n * i
+            temp.append(mul)
+        
+        final_array = sorted(temp)
+
+        for i in final_array:
+            if i % 2 ==0 and i != 0 :
+                return i
+                break
