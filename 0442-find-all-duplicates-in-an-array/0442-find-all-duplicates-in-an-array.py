84fcb2de7f8667f8944f519b3043b768d21e04e7
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2024-02-12 15:10:56 -0600

    Time: 258 ms (79.33%), Space: 24.8 MB (73.46%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        # Initialize an array to mark visited numbers
+        visited = [0] * (len(nums) + 1)
+        
+        # Mark visited numbers based on the elements in nums
+        for num in nums:
+            visited[num] += 1
+
+        # Find numbers that were visited more than once
+        duplicates = [i for i in range(1, len(nums) + 1) if visited[i] == 2]
+
+        return duplicates
